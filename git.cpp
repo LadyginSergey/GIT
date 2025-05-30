26360f93192a48610b606bfaa35f833c205df149
Ladygin Sergey <sergejladygin@gmail.com>
2025-05-31 04:56:29 +0500

    Добавьте файлы проекта.

---
+++ git.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+int main() {
+    setlocale(LC_ALL, "Russian");
+    int shifttime = 4000000;
+    int ordercustomer = 10;
+    int collectionorder = 40;
+    int totalclientsserved = shifttime / (ordercustomer * collectionorder);
+    std::cout << "Приветствуем вас в калькуляторе коммунальных услуг!\n";
+    std::cout << "Введите сумму, указанную в квитанции: " << shifttime << "\n";
+    std::cout << "Сколько подъездов в вашем доме? " << ordercustomer << "\n";
+    std::cout << "Сколько квартир в каждом подъезде? " << collectionorder << "\n";
+    std::cout << "-----Считаем-----\n";
+    std::cout << " Каждая квартира должна платить по " << totalclientsserved;
+}
